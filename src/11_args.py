dbc9e5b508bc4d60c0c7e452dded7fdb69e46061
jcs-lambda <57103874+jcs-lambda@users.noreply.github.com>
2020-06-01 15:12:36 -0500

    completed assignment 11

--- src/11_args.py
+++ src/11_args.py
@@ -5,6 +5,8 @@
 # the sum. This is what you'd consider to be a regular, normal function.
 
 # YOUR CODE HERE
+def f1(i: int, j: int):
+    return i + j
 
 print(f1(1, 2))
 
@@ -13,6 +15,14 @@ print(f1(1, 2))
 # Note: Google for "python arbitrary arguments" and look for "*args"
 
 # YOUR CODE HERE
+def f2(*args):
+    total = 0
+    for arg in args:
+        if isinstance(arg, int):
+            total = total + arg
+        elif isinstance(arg, list):
+            total = total + sum(arg)
+    return total
 
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
@@ -30,6 +40,11 @@ print(f2(a))    # Should print 22
 # Note: Google "python default arguments" for a hint.
 
 # YOUR CODE HERE
+def f3(i, j=None):
+    if j is None:
+        return i + 1
+    else:
+        return i + j
 
 print(f3(1, 2))  # Should print 3
 print(f3(8))     # Should print 9
@@ -44,6 +59,16 @@ print(f3(8))     # Should print 9
 # Note: Google "python keyword arguments".
 
 # YOUR CODE HERE
+def f4(*args, **kwargs):
+    for arg in args:
+        if isinstance(arg, dict):
+            for k, v in arg.items():
+                kwargs[k] = v
+    for k, v in kwargs.items():
+        if isinstance(v, str) and v.count(' ') > 0:
+            print(f'key: {k}, value: "{v}"')
+        else:
+            print(f'key: {k}, value: {v}')
 
 # Should print
 # key: a, value: 12
